d3e2f55e6c1be377070f8b539b575e9a76a49bf5
Atanas9708 <nasko_9708@mail.bg>
2018-01-21 13:43:53 +0200

    04

---
+++ 04. Basic Tree Data Structures - Tree, Binary Tree/Trees/BinaryTree.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+public class BinaryTree<T>
+{
+    public T Value { get; private set; }
+
+    public BinaryTree<T> Left { get; set; }
+    public BinaryTree<T> Right { get; set; }
+
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        this.Value = value;
+        this.Left = leftChild;
+        this.Right = rightChild;
+    }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        this.PrintIndentedPreOrder(this, indent);
+    }
+
+    private void PrintIndentedPreOrder(BinaryTree<T> node, int indent)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        Console.WriteLine($"{new string(' ', indent)}{node.Value}");
+        this.PrintIndentedPreOrder(node.Left, indent + 2);
+        this.PrintIndentedPreOrder(node.Right, indent + 2);
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        this.EachInOder(this, action);
+    }
+
+    private void EachInOder(BinaryTree<T>node, Action<T> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.EachInOder(node.Left, action);
+        action(node.Value);
+        this.EachInOder(node.Right, action);
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        this.EachPostOrder(this, action);
+    }
+
+    private void EachPostOrder(BinaryTree<T> node, Action<T> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.EachPostOrder(node.Left, action);
+        this.EachPostOrder(node.Right, action);
+        action(node.Value);
+    }
+}
